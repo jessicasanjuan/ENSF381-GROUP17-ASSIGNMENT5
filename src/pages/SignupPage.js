91f9183c20639cc9343abc5ba69774f7f285b958
jsanjuan <jessica.sanjuan@ucalgary.ca>
2025-04-04 17:12:30 -0600

    Added files
    added new files and new directory

---
+++ src/pages/SignupPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import RegForm from "../components/RegForm";
+
+const SignupPage = () => {
+  return (
+    <div>
+      <Header />
+      <RegForm />
+      <Footer />
+    </div>
+  );
+};
+
+export default LoginPage;
+
