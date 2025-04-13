e38c7208ca60d0c9991ddd7a8d6981fea58656e7
Crolu <chiruliacosta@gmail.com>
2025-04-12 19:22:02 -0600

    added requested styling on sign up page

--- src/components/RegForm.js
+++ src/components/RegForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import "../styles.css";
 
 function RegForm() {
   const [formData, setFormData] = useState({
@@ -58,45 +59,38 @@ function RegForm() {
   };
 
   return (
-    <div className="reg-form">
-      <h2>Signup</h2>
+    <div className="login-main">
+      <h2>Sign Up</h2>
       <form onSubmit={handleSubmit}>
-        <input name="username" placeholder="Username" value={formData.username} onChange={handleChange} />
-        <input name="password" type="password" placeholder="Password" value={formData.password} onChange={handleChange} />
-        <input name="confirmPassword" type="password" placeholder="Confirm Password" value={formData.confirmPassword} onChange={handleChange} />
-        <input name="email" type="email" placeholder="Email" value={formData.email} onChange={handleChange} />
-
-        <button
-          type="submit"
-          style={{
-            backgroundColor: '#4CAF50',
-            padding: '10px',
-            borderRadius: '5px',
-            margin: '10px',
-            opacity: 0.5,
-          }}
-          onMouseOver={(e) => {
-            e.target.style.backgroundColor = '#45A049';
-            e.target.style.opacity = 1.0;
-          }}
-          onMouseOut={(e) => {
-            e.target.style.backgroundColor = '#4CAF50';
-            e.target.style.opacity = 0.5;
-          }}
-        >
-          Signup
-        </button>
+        <div id="login-form">
+          <label for="username">Username:</label>
+          <input className='inputfield' id="username" name="username" placeholder="Username" value={formData.username} onChange={handleChange} />
+          <label for="password">Password:</label>
+          <input className='inputfield' id="password" name="password" type="password" placeholder="Password" value={formData.password} onChange={handleChange} />
+          <label for="confirm-password">Confirm Password:</label>
+          <input className='inputfield' id="confirm-password" name="confirmPassword" type="password" placeholder="Confirm Password" value={formData.confirmPassword} onChange={handleChange} />
+          <label for="email">Email:</label>
+          <input className='inputfield' id="email" name="email" type="email" placeholder="Email" value={formData.email} onChange={handleChange} />
+          <button type="submit" className='signup-button'>Sign Up</button>
+        </div>
+        
       </form>
 
       {errors.length > 0 && (
-        <div style={{ backgroundColor: '#f8d7da', padding: '10px', marginTop: '10px' }}>
-          <ul>
-            {errors.map((err, idx) => <li key={idx}>{err}</li>)}
-          </ul>
+        <div style={{ backgroundColor: 'white', padding: '10px', marginTop: '10px', border: '1px solid black', textAlign: 'center', width: '90%', marginLeft: '4%' }}>
+            {errors.map((err, idx) => <p>{err} </p>)}
         </div>
       )}
+      <br></br>
     </div>
   );
 }
 
 export default RegForm;
+
+
+/* old unordered list approach (changed to eliminate bullet points)
+<ul>
+{errors.map((err, idx) => <li key={idx}>{err}</li>)}
+</ul>
+*/
